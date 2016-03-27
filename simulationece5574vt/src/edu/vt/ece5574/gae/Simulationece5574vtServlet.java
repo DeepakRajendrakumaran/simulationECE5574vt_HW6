f209c7a1ecde4865ac9ec6ba5b4a2cae4d39a9ec
DuttaB <b.dutta.ee@gmail.com>
2016-03-27 18:38:19 -0400

    Creating simulation thread.

--- simulationece5574vt/src/edu/vt/ece5574/gae/Simulationece5574vtServlet.java
+++ simulationece5574vt/src/edu/vt/ece5574/gae/Simulationece5574vtServlet.java
@@ -8,16 +8,28 @@ import edu.vt.ece5574.sim.Simulation;
 @SuppressWarnings("serial")
 public class Simulationece5574vtServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		
-		Simulation sim = new Simulation(0);
-		String[] temp = new String[10];
+
 
 		// run is the entry point and is called with a list of strings
 		//similarly to how it could be called from the cmdline
 		//This is how doLoop is called so it's necessary to build it here 
 		//or have the simulation build its list
-		sim.run(temp);
-		
+
+		Runnable myRunnable = new Runnable(){		
+			public void run(){
+				Simulation sim = new Simulation(0);
+				String[] temp = new String[10];
+
+				System.out.println("Starting simulation Thread");
+
+				sim.run(temp);
+			}
+		};
+
+		Thread thread = new Thread(myRunnable);
+		thread.start();
+
+
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Simulation Started");
 	}
