0a02088e8e8c62b89f7dbe936edcdfe652325591
Deepak Rajendrakumaran <drajendrakumaran@gmail.com>
2016-03-28 23:30:58 -0400

    Adding simple Event framework

--- simulationece5574vt/src/edu/vt/ece5574/events/FireEvent.java
+++ simulationece5574vt/src/edu/vt/ece5574/events/FireEvent.java
@@ -5,12 +5,33 @@ package edu.vt.ece5574.events;
  * @author David Kindel
  *
  */
+/**
+ * Edit: Added simple flags to allow starting and stopping an event to allow sensor/robot interaction
+ * @author Deepak Rajendrakumaran
+ */
 public class FireEvent extends Event {
 
+	private boolean fireActive;
+	
 	public FireEvent() {
-		
+		fireActive = true;
+	}
+	
+	/**
+	 * used by sensor to check if even still active
+	 */
+	public boolean is_fireActive() {
+		return fireActive;
+	}
+	
+	/**
+	 * used to robot to signal that event has been dealt with
+	 */
+	public void turn_fireOff() {
+		 fireActive = false;
 	}
 
+	
 	public boolean init(String details){
 		if(super.setBaseDetails(details)){
 			return true;
--- simulationece5574vt/src/edu/vt/ece5574/events/MoveRobotEvent.java
+++ simulationece5574vt/src/edu/vt/ece5574/events/MoveRobotEvent.java
@@ -5,10 +5,31 @@ package edu.vt.ece5574.events;
  * @author David Kindel
  *
  */
+/**
+ * Edit: Added simple flags to allow starting and stopping an event to allow sensor/robot interaction
+ * @author Deepak Rajendrakumaran
+ */
 public class MoveRobotEvent extends Event{
 
+	
+	private boolean reachedLoc;
+	
 	public MoveRobotEvent() {
-		
+		reachedLoc = false;
+	}
+	
+	/**
+	 * used to check if even still active
+	 */
+	public boolean hasRobotReachedLoc() {
+		return reachedLoc;
+	}
+	
+	/**
+	 * used to robot to signal that event has been dealt with
+	 */
+	public void RobotReachedLoc() {
+		reachedLoc = true;
 	}
 
 	public boolean init(String details){
