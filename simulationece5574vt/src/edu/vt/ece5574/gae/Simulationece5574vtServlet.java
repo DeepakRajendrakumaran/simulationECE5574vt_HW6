2ac24cd532f1745e22769591b6c4d58707a5f569
DuttaB <b.dutta.ee@gmail.com>
2016-03-28 23:01:37 -0400

    Giving more interfaces to GAE servlet.

--- simulationece5574vt/src/edu/vt/ece5574/gae/Simulationece5574vtServlet.java
+++ simulationece5574vt/src/edu/vt/ece5574/gae/Simulationece5574vtServlet.java
@@ -1,6 +1,7 @@
 package edu.vt.ece5574.gae;
 import java.io.IOException;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
@@ -8,65 +9,104 @@ import edu.vt.ece5574.sim.Simulation;
 
 @SuppressWarnings("serial")
 public class Simulationece5574vtServlet extends HttpServlet {
+
+	private simRunnable runner;
+	private Thread simthread;
+	private Simulation sim;
+	private int isSimulatorRunning=0;
+
+	class simRunnable implements Runnable{		
+		public void run(){
+
+			System.out.println("Starting the Simulation Thread");
+			String[] temp = new String[0];
+
+			sim.run(temp);
+		}	
+	}
+
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		System.out.println("Initializing the simulator");
+
+		runner = new simRunnable();       
+		simthread = new Thread(runner);
+
+		sim = new Simulation(0);
+
+	}
+
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
-		if (req.getParameter("StartSim") != null) {
+		if (req.getParameter("StartSim") != null) {	
 
-		Runnable myRunnable = new Runnable(){		
-			public void run(){
-				Simulation sim = new Simulation(0);
-				String[] temp = new String[0];
-
-				System.out.println("Starting the Simulation Thread");
-
-				sim.run(temp);
+			if(isSimulatorRunning == 1)
+			{
+				System.out.println("Simulator is already running");				
+				resp.setContentType("text/plain");
+				resp.getWriter().println("Simulator is already running");
+			}else
+			{
+				simthread.start();
+				isSimulatorRunning=1;
+				System.out.println("Simulation Started");
+				resp.setContentType("text/plain");
+				resp.getWriter().println("Simulation Started");
 			}
-		};
+		}
 
-		Thread thread = new Thread(myRunnable);
-		thread.start();
+		if (req.getParameter("StopSim") != null) {
 
+			if(isSimulatorRunning == 0)
+			{
+				System.out.println("Simulator is already stopped");				
+				resp.setContentType("text/plain");
+				resp.getWriter().println("Simulator is already stopped");	
+			}else
+			{
+				System.out.println("Stopping the Simulation Thread");				
+				resp.setContentType("text/plain");
+				resp.getWriter().println("Stopping the Simulation Thread");
 
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Simulation Started");
-	}
-	
-	if (req.getParameter("StopSim") != null) {
-		
-		System.out.println("Stopping the Simulation Thread");
-		
-		//Call Stop API here
-		
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Simulation Stopped");
-		
-	}
+				isSimulatorRunning =0;
+				sim.end();
+			}
+		}
 
-	if (req.getParameter("ViewLogs") != null) {
-		
-		System.out.println("Opening the simulation log file");
-		
-		//Call the find the simulation file
-		
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Simulation log file contents");
-		
-	}
-	
+		if (req.getParameter("ViewLogs") != null) {
+
+			System.out.println("Opening the simulation log file");
+			resp.setContentType("text/plain");
+			resp.getWriter().println("Opening the simulation log file");
+
+			//Call the find the simulation file
+		}
+
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		super.doPost(req, resp);
-		
-		System.out.println("Uploaded the simulation Config file");
-		
-		//Call the find the simulation file
-		
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Simulation config file uploaded");
-		
+
+		if(isSimulatorRunning == 1)
+		{
+
+			System.out.println("Pls stop the simulator before config file upload");
+			resp.setContentType("text/plain");
+			resp.getWriter().println("Pls stop the simulator before config file upload");	
+
+		}
+		else
+		{
+			//Call the find the simulation file
+
+			System.out.println("Simulation config file uploaded");
+			resp.setContentType("text/plain");
+			resp.getWriter().println("Simulation config file uploaded");
+		}
+
 	}	
-	
+
 }
