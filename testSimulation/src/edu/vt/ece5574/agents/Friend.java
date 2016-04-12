76f10741ec68ccf09d22961ffbb5b581e0168a74
vedahari <vedahari@vt.edu>
2016-04-11 22:35:18 -0400

    Create Friend.java

---
+++ testSimulation/src/edu/vt/ece5574/agents/Friend.java
@@ -0,0 +1,18 @@
+package edu.vt.ece5574.agents;
+
+public class Friend extends User {
+
+	private static final long serialVersionUID = 1L;
+
+	public Friend(String id, String buildingID) {
+		// TODO Auto-generated constructor stub
+		//Adults are always assumed to have app
+		super(id,buildingID,true);
+	}
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		super.move();
+	}
+}
