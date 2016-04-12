d3ea0d9e1ac0d0b16b79f142a671b5bfc624afb4
vedahari <vedahari@vt.edu>
2016-04-11 22:33:56 -0400

    Create Child.java

---
+++ testSimulation/src/edu/vt/ece5574/agents/Child.java
@@ -0,0 +1,19 @@
+package edu.vt.ece5574.agents;
+
+public class Child extends User{
+	
+	private static final long serialVersionUID = 1L;
+
+	public Child(String id, String buildingID, boolean bAppUser) {
+		// TODO Auto-generated constructor stub
+		//Adults are always assumed to have app
+		super(id,buildingID,true);
+	}
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		super.move();
+	}
+
+}
