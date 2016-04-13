4684e11d2edc3d2982bb2a582e4df790b6dd20c7
ask <ameyask@vt.edu>
2016-04-13 16:54:15 -0400

    uncommented two robots. for people who wish to see UI running

--- testSimulation/src/edu/vt/ece5574/sim/SimulationWithUI.java
+++ testSimulation/src/edu/vt/ece5574/sim/SimulationWithUI.java
@@ -56,8 +56,8 @@ public class SimulationWithUI extends GUIState
      {
 		 Simulation sim = (Simulation)state;
 		 Building building = (Building)sim.getAgentByID("0");
-	//	 Robot robot0 = building.createRobot(); //robot0 is not being used here. but we may find use for it.
-	//	 Robot robot1 = building.createRobot();
+		 Robot robot0 = building.createRobot(); //robot0 is not being used here. but we may find use for it.
+		 Robot robot1 = building.createRobot();
 	//	 User user0 = building.createUser();
 		//movement gives errors when 2 robots are added.
 
