ad270b701597d8cf48628e8ab49cf930b25845cf
ask <ameyask@vt.edu>
2016-04-18 19:00:23 -0400

    reverting to boolean Smoke

--- testSimulation/src/edu/vt/ece5574/agents/SmokeSensor.java
+++ testSimulation/src/edu/vt/ece5574/agents/SmokeSensor.java
@@ -1,41 +1,6 @@
 package edu.vt.ece5574.agents;
 
-<<<<<<< HEAD
-import edu.vt.ece5574.roomconditions.Temperature;
-import edu.vt.ece5574.sim.Simulation;
-import sim.engine.SimState;
 
-// -------------------------------------------------------------------------
-/**
- *  Senses the prescence of smoke.
- *
- *  @author Stephanie
- *  @version Apr 18, 2016
- */
-public class SmokeSensor extends Sensor
-{
-
-    private static final long serialVersionUID = 1;
-    int x,y;
-    Building bld;
-    //Smoke smoke;
-
-    public SmokeSensor(String id_, String buildingID_, String sensorType_)
-    {
-        super(id_, buildingID_, "smoke");
-        // TODO Auto-generated constructor stub
-    }
-
-    public SmokeSensor(String id_, String buildingID_,SimState state_, int x_, int y_) {
-
-        super(id_,buildingID_,"weight");
-        Simulation state = (Simulation)state_;
-        x = x_;
-        y = y_;
-        bld = (Building)state.getAgentByID(buildingID);
-        //smoke = bld.getRoomSmokeById(bld.getRoomId(x,y));
-    }
-=======
 
 import edu.vt.ece5574.events.Event;
 import edu.vt.ece5574.events.FireEvent;
@@ -119,6 +84,5 @@ public class SmokeSensor extends Sensor {
 		}		
 	}
 
->>>>>>> 3b0ff2cd9b8bcf153ba1cd73bf45584debec42b2
 
 }
--- testSimulation/src/edu/vt/ece5574/roomconditions/Smoke.java
+++ testSimulation/src/edu/vt/ece5574/roomconditions/Smoke.java
@@ -1,63 +1,5 @@
 package edu.vt.ece5574.roomconditions;
-<<<<<<< HEAD
 
-import edu.vt.ece5574.sim.Simulation;
-import java.util.concurrent.atomic.AtomicInteger;
-import sim.engine.SimState;
-
-// -------------------------------------------------------------------------
-/**
- *  Represents smoke presence.
- *
- *  @author Stephanie
- *  @version Apr 18, 2016
- */
-public class Smoke
-{
-    protected String buildingID = "0"; //a building will have the same ID as building ID
-    protected String roomID;
-
-    private AtomicInteger smokeLevel;
-    private Simulation state;
-
-    public Smoke(SimState state_){
-        state = (Simulation)state_;
-        smokeLevel = new AtomicInteger(0);
-    }
-
-    public Smoke(int smoke_,SimState state_){
-        state = (Simulation)state_;
-        smokeLevel = new AtomicInteger(smoke_);
-    }
-
-    public int getSmokeLevel(){
-
-        return smokeLevel.get();
-    }
-
-    public void defSmokeChange(){
-
-        int val = smokeLevel.get();
-
-        int prev = val;
-        val = (((state.random.nextInt()%10) + 100)* val)/100  ;
-        if(val < 75 || val > 10000){
-            val = prev;
-        }
-        smokeLevel.set(val);
-    }
-
-    public void fireSmokeChange(int severity){
-
-        int val = smokeLevel.get();
-
-        if(val < 10000){
-            val = val + severity*100;
-        }
-        smokeLevel.set(val);
-    }
-
-=======
 import java.util.concurrent.atomic.AtomicBoolean;
 import edu.vt.ece5574.sim.Simulation;
 import sim.engine.SimState;
@@ -116,5 +58,4 @@ public class Smoke  {
 
 	}
 */
->>>>>>> 3b0ff2cd9b8bcf153ba1cd73bf45584debec42b2
 }
